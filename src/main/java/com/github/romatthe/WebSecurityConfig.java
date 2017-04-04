b135a5563bfc7d5f5a170e9d6fa749e1ef80b2fa
romatthe <robin.mattheussen@gmail.com>
2017-04-04 23:58:36 +0200

    Configured example JwtAuthenticationFilter

--- src/main/java/com/github/romatthe/WebSecurityConfig.java
+++ src/main/java/com/github/romatthe/WebSecurityConfig.java
@@ -4,17 +4,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.SecurityConfig;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)  // <--- Enable @PreFilter, @PreAuthorize, @PostFilter, @PostAuthorize beans
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UnauthenticatedHandler unauthenticatedHandler;
+
+    @Autowired
+    private JwtAuthenticationTokenFilter jwtAuthenticationTokenFilter;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -33,6 +39,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .antMatchers("/api/login").permitAll()
             // Do not permit unauthoried requests to any other API endpoints
             .antMatchers("api/**").permitAll().anyRequest().authenticated();
+
+        http
+            .addFilterBefore(jwtAuthenticationTokenFilter, UsernamePasswordAuthenticationFilter.class);
+            // --> Can only insert before or after an already defined filter
+            //     Not sure if this is the right place
     }
 
     @Override
--- src/main/java/com/github/romatthe/controllers/LogInController.java
+++ src/main/java/com/github/romatthe/controllers/LogInController.java
@@ -4,13 +4,24 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 @RestController
 @RequestMapping(path = "/api")
 public class LogInController {
 
     @PostMapping(path = "/login")
-    public String logIn() {
-        return "ACCESS GRANTED, NUKES LAUNCHED";
+    public String logIn() throws IOException, URISyntaxException {
+        URI uri = getClass().getClassLoader().getResource("response.txt").toURI();
+        List<String> lines = Files.readAllLines(Paths.get(uri), Charset.defaultCharset());
+
+        return String.join("\n", lines);
     }
 
 }
